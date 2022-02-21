06ccc80d586b18a6a23de023eb9326d86c92c78f
dvthnight <davide.lanotte@protonmail.com>
2022-02-21 16:55:20 +0100

    completato esercizio con oggetti

--- app.js
+++ app.js
@@ -25,33 +25,33 @@
 
 const paese = [
     {
-        item : 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        item : "img/01.jpg",
+        title: "Svezia",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
     },
 
     {
-        item : 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum',
+        item : "img/02.jpg",
+        title: "Svizzera",
+        text: "Lorem ipsum",
     },
 
     {
-        item : 'img/03.jpg',
-        title:  'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        item : "img/03.jpg",
+        title:  "Gran Bretagna",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
     },
 
     {
-        item : 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+        item : "img/04.jpg",
+        title: "Germania",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
     },
 
     {
-        item : 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+        item : "img/05.jpg",
+        title: "Paradise",
+        text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
     },
 ]
 
@@ -61,11 +61,13 @@ const contenitoreAnteprime = [];
 
 let fotoCorrente = 0;
 
-for(let i = 0; i < items.length; i++){
+for(let i = 0; i < paese.length; i++){
     const anteprima = document.createElement("figure");
     const anteprimaImg = document.createElement("img");
+
+    console.log(paese, paese[i].item)
     
-    anteprimaImg.src = items[i];
+    anteprimaImg.src = paese[i].item;
     anteprima.append(anteprimaImg);
     // console.dir(anteprimaImg);
 
@@ -80,10 +82,10 @@ for(let i = 0; i < items.length; i++){
         fotoCorrente = i;
         contenitoreAnteprime[fotoCorrente].classList.add("active");
 
-        fotoPrincipale.src = items[fotoCorrente];
+        fotoPrincipale.src = paese[fotoCorrente].item;
         contenitorePrincipale.append(fotoPrincipale);
-        titoloFoto.innerHTML = title[fotoCorrente];
-        paragrafoFoto.innerHTML = text[fotoCorrente];
+        titoloFoto.innerHTML = paese[fotoCorrente].title;
+        paragrafoFoto.innerHTML = paese[fotoCorrente].text;
 
     })
 
@@ -101,36 +103,36 @@ const contenitorePrincipale = document.getElementById("contenitore-principale");
 const descrizione = document.querySelector(".descrizione");
 
 const fotoPrincipale = document.createElement("img");
-fotoPrincipale.src = items[fotoCorrente];
+fotoPrincipale.src = paese[fotoCorrente].item;
 contenitorePrincipale.append(fotoPrincipale);
 
 const titoloFoto = document.getElementById("titolo-foto");
 const paragrafoFoto = document.getElementById("paragrafo-foto");
 
-titoloFoto.innerHTML = title[fotoCorrente];
-paragrafoFoto.innerHTML = text[fotoCorrente];
+titoloFoto.innerHTML = paese[fotoCorrente].title;
+paragrafoFoto.innerHTML = paese[fotoCorrente].text;
 
 
 arrowDown.addEventListener("click", function(){
-    if(fotoCorrente < items.length -1){
+    if(fotoCorrente < paese.length -1){
         contenitoreAnteprime[fotoCorrente].classList.remove("active");
         fotoCorrente++;
         contenitoreAnteprime[fotoCorrente].classList.add("active");
    
-        fotoPrincipale.src = items[fotoCorrente];
+        fotoPrincipale.src = paese[fotoCorrente].item;
         contenitorePrincipale.append(fotoPrincipale);
-        titoloFoto.innerHTML = title[fotoCorrente];
-        paragrafoFoto.innerHTML = text[fotoCorrente];
+        titoloFoto.innerHTML = paese[fotoCorrente].title;
+        paragrafoFoto.innerHTML = paese[fotoCorrente].text;
     } else{
         
         contenitoreAnteprime[fotoCorrente].classList.remove("active");
         fotoCorrente = 0;
         contenitoreAnteprime[fotoCorrente].classList.add("active");
    
-        fotoPrincipale.src = items[fotoCorrente];
+        fotoPrincipale.src = paese[fotoCorrente].item;
         contenitorePrincipale.append(fotoPrincipale);
-        titoloFoto.innerHTML = title[fotoCorrente];
-        paragrafoFoto.innerHTML = text[fotoCorrente];
+        titoloFoto.innerHTML = paese[fotoCorrente].title;
+        paragrafoFoto.innerHTML = paese[fotoCorrente].text;
     }
 })
 
@@ -142,18 +144,18 @@ arrowUp.addEventListener("click", function(){
         fotoCorrente--;
         contenitoreAnteprime[fotoCorrente].classList.add("active");
     
-        fotoPrincipale.src = items[fotoCorrente];
+        fotoPrincipale.src = paese[fotoCorrente].item;
         contenitorePrincipale.append(fotoPrincipale);
-        titoloFoto.innerHTML = title[fotoCorrente];
-        paragrafoFoto.innerHTML = text[fotoCorrente];
+        titoloFoto.innerHTML = paese[fotoCorrente].title;
+        paragrafoFoto.innerHTML = paese[fotoCorrente].text;
     }else{
         contenitoreAnteprime[fotoCorrente].classList.remove("active");
-        fotoCorrente = items.length-1;
+        fotoCorrente = paese.length-1;
         contenitoreAnteprime[fotoCorrente].classList.add("active");
     
-        fotoPrincipale.src = items[fotoCorrente];
+        fotoPrincipale.src = paese[fotoCorrente].item;
         contenitorePrincipale.append(fotoPrincipale);
-        titoloFoto.innerHTML = title[fotoCorrente];
-        paragrafoFoto.innerHTML = text[fotoCorrente];
+        titoloFoto.innerHTML = paese[fotoCorrente].title;
+        paragrafoFoto.innerHTML = paese[fotoCorrente].text;
     }
 })
